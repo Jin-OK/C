87bfeb026582506932ec2f31c064255cce593d4d
Jin-OK <daxueyoujian2021@163.com>
2022-09-19 14:08:24 +0800

    file

--- TEST.c
+++ TEST.c
@@ -1,37 +1,6 @@
-#include<stdio.h>
-#include<math.h>
-char key[100000001];             
-long long z,flag = 3,N,result=0;
-
-void Change(long long num){    
-    long long op = num;
-    do{
-        key[op] = '}';
-        op+=num;
-    } while (op<=N);
-}
-
-void SU(long long num){ 
-    long long len = sqrt(num)+1;
-    while (flag<=len){
-        if(num%len==0) return ;
-        flag+=2;
-    }
-    Change(num);
-    result++;
-}
-
-int main(void){
-
-    scanf("%lld",&N);
-    if(N>=2) {
-        result++;
-        Change(2);
-    }
-    for(z=3;z<=N;z+=2){
-        if(key[z]=='}') continue;
-        SU(z);
-    }
-    printf("%lld",result);
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
     return 0;
 }
